ec76122e9cf90615b98a6e7e7dbc9bd35db2254d
250541067-design <250541067@firat.edu.tr>
2025-10-22 10:45:34 +0300

    Update BesKereTekrar.java

---
+++ Task-1-HosgeldinMesajlari/BesKereTekrar.java
@@ -0,0 +1,9 @@
+public class beskeretekrar{
+    public static void main(String[] args) {
+        System.out.println("Java'ya hoş geldiniz");
+        System.out.println("Java'ya hoş geldiniz");
+        System.out.println("Java'ya hoş geldiniz");
+        System.out.println("Java'ya hoş geldiniz");
+        System.out.println("Java'ya hoş geldiniz");
+    }
+}
