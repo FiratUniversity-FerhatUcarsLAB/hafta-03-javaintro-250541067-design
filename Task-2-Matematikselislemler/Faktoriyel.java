74dd9849846b09eb5c4d00cd1e76152788f5cfd0
250541067-design <250541067@firat.edu.tr>
2025-10-23 16:05:05 +0300

    Update Faktoriyel.java

---
+++ Task-2-Matematikselislemler/Faktoriyel.java
@@ -0,0 +1,7 @@
+public class faktoriyel {
+    public static void main(String[] args) {
+        int sayı = 5;
+        int faktoriyel = 1*2*3*4*5;
+        System.out.println(sayı+ "!=" + faktoriyel);
+       }
+}
