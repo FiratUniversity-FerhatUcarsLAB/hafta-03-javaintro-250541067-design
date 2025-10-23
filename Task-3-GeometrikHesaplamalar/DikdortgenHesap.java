fee496555dc93881dfa31918316851662c137b44
250541067-design <250541067@firat.edu.tr>
2025-10-23 17:02:54 +0300

    Update DikdortgenHesap.java

---
+++ Task-3-GeometrikHesaplamalar/DikdortgenHesap.java
@@ -0,0 +1,15 @@
+public class Dikdortgen {
+    public static void main(String[] args) {
+        double en = 4.5;
+        double boy = 7.9;
+
+        // Alan = en * boy
+        double alan = en * boy;
+
+        // Çevre = 2 * (en + boy)
+        double cevre = 2 * (en + boy);
+
+        System.out.println("Dikdörtgenin alanı: " + alan);
+        System.out.println("Dikdörtgenin çevresi: " + cevre);
+    }
+}
