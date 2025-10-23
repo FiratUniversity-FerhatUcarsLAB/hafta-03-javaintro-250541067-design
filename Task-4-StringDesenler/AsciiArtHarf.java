e87058f8bf46606719f2e5c6bfd2ba4b8af3db87
250541067-design <250541067@firat.edu.tr>
2025-10-23 18:09:07 +0300

    Update AsciiArtHarf.java

---
+++ Task-4-StringDesenler/AsciiArtHarf.java
@@ -0,0 +1,11 @@
+public class ascıı {
+    public static void main(String[] args) {
+        System.out.println("PPPP");
+        System.out.println("P   P");
+        System.out.println("P   P");
+        System.out.println("PPPP");
+        System.out.println("P");
+        System.out.println("P");
+        System.out.println("P");
+    }
+}
