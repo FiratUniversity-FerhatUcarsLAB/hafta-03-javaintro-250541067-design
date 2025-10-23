b9037834a2d430cb3e8b8db71e760af996025aa9
250541067-design <250541067@firat.edu.tr>
2025-10-23 17:52:44 +0300

    Update IsimKarti.java

---
+++ Task-4-StringDesenler/IsimKarti.java
@@ -0,0 +1,14 @@
+public class isimkartı {
+    public static void main(String[] args) {
+        String ad = "Avşin Pelin";
+        String soyad = "Bilgiç";
+        String ogrenciNo = "250541067";
+        String bolum = "Yazılım Mühendisliği";
+
+        System.out.println("======İSİM KARTI======");
+        System.out.println("Ad        : " + ad);
+        System.out.println("Soyad     : " + soyad);
+        System.out.println("Öğrenci No: " + ogrenciNo);
+        System.out.println("Bölüm     : " + bolum);
+    }
+}
